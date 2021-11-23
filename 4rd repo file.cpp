2533816ca230a419f3e2782b0e3f3903510be994
abdulhamid100 <ahmadhamid040@gmail.com>
2021-11-23 21:44:56 +0600

    done work

--- 4rd repo file.cpp
+++ 4rd repo file.cpp
@@ -11,6 +11,10 @@ int main()
     cout<<"Hello world\n";
 
     //2nd time push into repository
+
+    cout<<"Practice, Practice, Practice"<<endl;
+    cout<<"you know that Practice maximum perfect\n";
+    cout<<"work hard and shine your life and don't hard others and pray to almighty Allah\n";
     
     
 
